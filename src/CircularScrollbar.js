21819d44232df48ca348a11b9b621a8e0c64c59d
wsuyash <suyashcr07@gmail.com>
2021-12-24 12:42:17 +0530

    Design Display component

--- src/CircularScrollbar.js
+++ src/CircularScrollbar.js
@@ -1,13 +1,13 @@
 function CircularScrollbar() {
 	return (
-		<div className='CircularScrollbar'>
+		<div className='CircularScrollbar' id="circular-scroll">
 			<span id='menu-button'>MENU</span>
-			<i class="fa-solid fa-backward-fast" id='backward-button'></i>
+			<i className="fa-solid fa-backward-fast nav-btn" id='backward-button'></i>
 			<div id='play-pause-wrapper'>
-				<i class="fa-solid fa-play" id='play-button'></i>
-				<i class="fa-solid fa-pause" id='pause-button'></i>
+				<i className="fa-solid fa-play nav-btn" id='play-button'></i>
+				<i className="fa-solid fa-pause nav-btn" id='pause-button'></i>
 			</div>
-			<i class="fa-solid fa-forward-fast" id='forward-button'></i>
+			<i className="fa-solid fa-forward-fast nav-btn" id='forward-button'></i>
 			<div id='enter-button'></div>
 		</div>
 	);
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,27 @@
 function Display() {
 	return (
 		<div className='Display'>
-			<h1>Display</h1>
+			<div id="ipod-main-menu">
+				<div id="ipod-main-menu-title">iPod</div>
+				<ul id="menu-items-list">
+					<li className="menu-item-wrapper active">
+						<p className="menu-item-name">Cover Flow</p>
+						<i className="fas fa-greater-than"></i>
+					</li>
+					<li className="menu-item-wrapper">
+						<p className="menu-item-name">Music</p>
+						<i className="fas fa-greater-than"></i>
+					</li>
+					<li className="menu-item-wrapper">
+						<p className="menu-item-name">Games</p>
+						<i className="fas fa-greater-than"></i>
+					</li>
+					<li className="menu-item-wrapper">
+						<p className="menu-item-name">Settings</p>
+						<i className="fas fa-greater-than"></i>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
